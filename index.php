41c3b206bffc57cbbd9f799a08d10abdeeec136f
Azhar Mehmood <azharhrp7171@gmail.com>
2021-05-12 23:02:30 +0500

    update index file

--- index.php
+++ index.php
@@ -1,6 +1,157 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <!--=============== basic  ===============-->
+        <meta charset="UTF-8">
+        <title>Zafran</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="robots" content="index, follow"/>
+        <meta name="keywords" content=""/>
+        <meta name="description" content=""/>
+        <!--=============== css  ===============--> 
+        <link type="text/css" rel="stylesheet" href="css/reset.css">
+        <link type="text/css" rel="stylesheet" href="css/plugins.css">
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <link type="text/css" rel="stylesheet" href="css/color.css">
+        <!--=============== favicons ===============-->
+        <link rel="shortcut icon" href="images/favicon.ico">
+    </head>
+    <body>
+        <!-- lodaer  -->
+        <div class="loader-wrap">
+            <div class="loader-item">
+                <div class="cd-loader-layer" data-frame="25">
+                    <div class="loader-layer"></div>
+                </div>
+                <span class="loader"></span>
+            </div>
+        </div>
+        <!-- loader end  -->
+        <!-- main start  -->
+        <div id="main">
+            <header class="main-header">
+                <!-- header-top  -->
+                <div class="header-top">
+                    <div class="container">
+                        <div class="lang-wrap"><a href="#" class="act-lang">En</a><span>/</span><a href="#">Fr</a></div>
+                        <div class="header-top_contacts"><a href="#"><span>Call now:</span> 980-258-0651</a><a href="#"><span>Write :</span> info@zafran.com</a></div>
+                    </div>
+                </div>
+                <!--header-top end -->
+                <!-- header-inner -->
+                <div class="header-inner  fl-wrap">
+                    <div class="container">
+                        <div class="header-container fl-wrap">
+                            <a href="index.php" class="logo-holder"><img src="images/logo.png" alt=""></a>
+                            <div class="show-reserv_button show-rb"><span>Reservation</span> <i class="fal fa-bookmark"></i></div>
+                            <div class="show-share-btn showshare htact"><i class="fal fa-bullhorn"></i> <span class="header-tooltip">Share</span></div>
+                            <div class="show-cart sc_btn htact"><i class="fal fa-shopping-bag"></i><span class="show-cart_count">3</span><span class="header-tooltip">Your Cart</span></div>
+                            <!-- nav-button-wrap-->
+                            <div class="nav-button-wrap">
+                                <div class="nav-button">
+                                    <span></span><span></span><span></span>
+                                </div>
+                            </div>
+                            <!-- nav-button-wrap end-->
+                            <!--  navigation -->
+                            <div class="nav-holder main-menu">
+                                <nav>
+                                    <ul>
+                                        <li>
+                                            <a href="index.php" class="act-link">Home </a>
+                                            <!--second level -->
+                                            
+                                            <!--second level end-->
+                                        </li>
+                                        <li>
+                                            <a href="menu.php">Menu<i class="act-link"></i></a>
+                                            
+                                        </li>
 
-            <!-- header  -->
-            <?php include 'header.php'; ?>
+                                        <li><a href="gallery.php">Gallery</a></li>
+                                        <li>
+                                         <a href="#">Catering<i class="fas fa-caret-down"></i></a>
+                                         <ul>
+                                                
+                                                <li><a href="https://www.ezcater.com/brand/pvt/zafran-kabab-palace" target="_blank">Order Catering</a></li>
+                                            </ul>
+                                        </li>
+                                        
+                                        <li><a href="about.php">About</a></li>
+                                        <li><a href="contact.php">Contact</a></li>
+                                        <!-- <li><a href="blog.php">News</a></li> -->
+                                        <li>
+                                            <a href="#">Pages<i class="fas fa-caret-down"></i></a>
+                                            <!--second level -->
+                                            <ul>
+                                                <li><a href="shop.php">Shop</a></li>
+                                                <li><a href="product-single.php">Product Single</a></li>
+                                                <li><a href="cart.php">Cart</a></li>
+                                                <li><a href="gallery.php">Gallery</a></li>
+                                                <li><a href="blog-single.php">Blog single</a></li>
+                                                <li><a href="404.php">404</a></li>
+                                                <li><a href="coming-soon.php">Coming Soon</a></li>
+                                            </ul>
+                                            <!--second level end-->
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <!-- navigation  end -->                        
+                            <!-- header-cart_wrap  -->
+                            <div class="header-cart_wrap novis_cart">
+                                <div class="header-cart_title">Your Cart <span>4 items</span></div>
+                                <div class="header-cart_wrap_container fl-wrap">
+                                    <div class="box-widget-content">
+                                        <div class="widget-posts fl-wrap">
+                                            <ol>
+                                                <li class="clearfix">
+                                                    <a href="#"  class="widget-posts-img"><img src="images/menu/1.jpg" class="respimg" alt=""></a>
+                                                    <div class="widget-posts-descr">
+                                                        <a href="#" title="">Grilled Steaks</a>
+                                                        <div class="widget-posts-descr_calc clearfix">1 <span>x</span> $45</div>
+                                                    </div>
+                                                    <div class="clear-cart_button"><i class="fal fa-times"></i></div>
+                                                </li>
+                                                <li class="clearfix">
+                                                    <a href="#"  class="widget-posts-img"><img src="images/menu/1.jpg" class="respimg" alt=""></a>
+                                                    <div class="widget-posts-descr">
+                                                        <a href="#" title="">Cripsy Lobster & Shrimp Bites</a>
+                                                        <div class="widget-posts-descr_calc clearfix">2 <span>x</span> $22</div>
+                                                    </div>
+                                                    <div class="clear-cart_button"><i class="fal fa-times"></i></div>
+                                                </li>
+                                                <li class="clearfix">
+                                                    <a href="#"  class="widget-posts-img"><img src="images/menu/1.jpg" class="respimg" alt=""></a>
+                                                    <div class="widget-posts-descr">
+                                                        <a href="#" title="">Chicken tortilla soup</a>
+                                                        <div class="widget-posts-descr_calc clearfix">1 <span>x</span> $37</div>
+                                                    </div>
+                                                    <div class="clear-cart_button"><i class="fal fa-times"></i></div>
+                                                </li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="header-cart_wrap_total fl-wrap">
+                                    <div class="header-cart_wrap_total_item">Subtotal : <span>$147</span></div>
+                                </div>
+                                <div class="header-cart_wrap_footer fl-wrap">
+                                    <a href="cart.php"> View Cart</a>
+                                    <a href="checkout.php">  Checkout</a>
+                                </div>
+                            </div>
+                            <!-- header-cart_wrap end  -->
+                            <!-- share-wrapper -->
+                            <div class="share-wrapper isShare">
+                                <div class="share-container fl-wrap"></div>
+                            </div>
+                            <!-- share-wrapper-end -->
+                        </div>
+                    </div>
+                </div>
+                <!-- header-inner end  -->
+            </header>
             <!--header end -->	
             <!-- wrapper  -->	
             <div id="wrapper">
